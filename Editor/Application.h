9ca553277d7ad3d459846353da8996a68fe59cc1
Adria Pons <adria.pons.mensa@estudiantat.upc.edu>
2023-10-18 19:09:07 +0200

    Project rename

---
+++ Editor/Application.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <list>
+#include "Globals.h"
+#include "Module.h"
+#include "ModuleWindow.h"
+#include "ModuleInput.h"
+#include "ModuleRenderer.h"
+#include "ModuleUI.h"
+#include "..\Engine\GameEngine.h"
+
+class Application
+{
+public:
+	ModuleWindow* window;
+	ModuleInput* input;
+	ModuleRenderer* renderer;
+	ModuleUI* ui;
+
+	GameEngine* gEngine;
+
+private:
+
+	std::list<Module*> list_modules;
+
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+private:
+
+	void AddModule(Module* mod);
+	void PrepareUpdate();
+	void FinishUpdate();
+};
