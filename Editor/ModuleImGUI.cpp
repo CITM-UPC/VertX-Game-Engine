39b9c63fd6b4693589a430c7001a1f700e4f7e71
Adria Pons <adria.pons.mensa@estudiantat.upc.edu>
2023-10-22 00:15:16 +0200

    Option to Quit + Inspector, Assets & Hierarchy windows

--- Editor/ModuleImGUI.cpp
+++ Editor/ModuleImGUI.cpp
@@ -17,30 +17,37 @@ ModuleImGUI::~ModuleImGUI()
 
 bool ModuleImGUI::Init()
 {
-	LOG("Creating UI");
+	LOG("Initializing ImGui Module");
 
+	// Setup ImGui context
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+	ImGuiIO& io = ImGui::GetIO(); 
 
+	// Enable keyboard
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	// Enable gamepad
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	// Enable docking
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+	// Setup Renderer bindings
 	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->game_engine->renderer3D_engine->context);
 	ImGui_ImplOpenGL3_Init();
+
+	// Setup ImGui colors style
+	ImGui::StyleColorsClassic();
+	//ImGui::StyleColorsDark();
 
 	return true;
 }
 
 update_status ModuleImGUI::PreUpdate()
 {
-	//This here does not work. Currently in Input.cpp
-	//ImGui_ImplSDL2_ProcessEvent(&pollevent); 
-
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplSDL2_NewFrame();
 	ImGui::NewFrame();
-
+	
 	#pragma region	ImGui_MenuBar_Test
 
 	if (dockSpaceEnabled)
@@ -54,7 +61,6 @@ update_status ModuleImGUI::PreUpdate()
 	{
 		if (ImGui::BeginMenu("File"))
 		{
-			//ImGui::PlotHistogram();
 			if (ImGui::MenuItem("New", "Not implemented")) {}
 			if (ImGui::MenuItem("Open", "Not implemented")) {}
 			if (ImGui::BeginMenu("Open Recent", "Not implemented"))
@@ -109,6 +115,13 @@ update_status ModuleImGUI::PreUpdate()
 			}
 			ImGui::EndMenu();
 		}
+		if (ImGui::BeginMenu("Quit"))
+		{
+			if (ImGui::MenuItem("Close the VertX Game Engine"))
+				return UPDATE_STOP;
+
+			ImGui::EndMenu();
+		}
 		ImGui::EndMainMenuBar();
 	}
 
@@ -127,48 +140,39 @@ update_status ModuleImGUI::PreUpdate()
 	if (about) {
 		ImGui::Begin("About");
 		
-		ImGui::Text("This Engine of Mine v0.1\nA 3D Game Engine for the Game Engines subject");
-		ImGui::Text("By Jonathan Cacay & Ethan Martin\n3rd Party Libraries used :");
-		ImGui::Text("LIBRARIES HERE");
-		ImGui::Text("License:\nMIT License\nCopyright(c) 2023 Jonathan Cacay & Ethan Martin");
-		ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the Software), \nto deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, \nand /or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions : ");
-		ImGui::Text("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.");
-		ImGui::Text("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, \nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER \nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS \nIN THE SOFTWARE.");
-		
+
 		ImGui::End();
 	}
 	if (demoWindow)
-		ImGui::ShowDemoWindow(); // Show demo window! :)
+		ImGui::ShowDemoWindow();
 
-	#pragma region ImGui_Windows_Test
-
-	ImGui::Begin("Window A");
-	ImGui::Text("This is window A");
-	ImGui::Button("Button on window A");
+	
+	// Create Inspector window
+	ImGui::Begin("Inspector");
+	
 	ImGui::End();
 
-	ImGui::Begin("Window B");
-	ImGui::Text("This is window B");
+	
+	// Create Assets window
+	ImGui::Begin("Assets");
+
 	ImGui::End();
 
-	ImGui::Begin("Window B");
-	ImGui::Button("Button on window B");
+
+	// Create Hierarchy window
+
+	ImGui::Begin("Hierarchy");
+
 	ImGui::End();
 
-	ImGui::Begin("Window C");
-	ImGui::Text("This is window C");
-	if (ImGui::Button("Button on window C")) { LOG("Button on window C pressed"); }
-	if (ImGui::Button("Close editor")) { return UPDATE_STOP; }
-	ImGui::End();
 
-	#pragma endregion
 
 	return UPDATE_CONTINUE;
 }
 
 bool ModuleImGUI::CleanUp()
 {
-	LOG("Destroying UI");
+	LOG("Destroying ImGUI");
 
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplSDL2_Shutdown();
